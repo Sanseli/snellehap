326385f81f9ab252118e3cb45cc840b5501037ea
Lisa Sansen <lisasansen97@gmail.com>
2022-03-30 20:08:00 +0200

    add body

--- pages/webshop.vue
+++ pages/webshop.vue
@@ -3,22 +3,24 @@
     <head>
       <title></title>
     </head>
-    <div id="my-store-71815548"></div>
-    <div>
-      <script
-        type="text/javascript"
-        src="https://app.shopsettings.com/script.js?71815548&data_platform=code&data_date=2017-10-02"
-        charset="utf-8"
-      ></script>
-      <script type="text/javascript">
-        xProductBrowser(
-          "categoriesPerRow=3",
-          "views=grid(20,3) list(60) table(60)",
-          "categoryView=grid",
-          "searchView=list",
-          "id=my-store-71815548"
-        );
-      </script>
-    </div>
+    <body>
+      <div id="my-store-71815548"></div>
+      <div>
+        <script
+          type="text/javascript"
+          src="https://app.shopsettings.com/script.js?71815548&data_platform=code&data_date=2017-10-02"
+          charset="utf-8"
+        ></script>
+        <script type="text/javascript">
+          xProductBrowser(
+            "categoriesPerRow=3",
+            "views=grid(20,3) list(60) table(60)",
+            "categoryView=grid",
+            "searchView=list",
+            "id=my-store-71815548"
+          );
+        </script>
+      </div>
+    </body>
   </div>
 </template>

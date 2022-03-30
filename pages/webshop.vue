b331c4d1406f57971dc01c7d3dd3de8fe7b0da41
Lisa Sansen <lisasansen97@gmail.com>
2022-03-30 19:55:54 +0200

    create webshop page

---
+++ pages/webshop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div id="my-store-71815548"></div>
+    <div>
+      <script
+        type="text/javascript"
+        src="https://app.shopsettings.com/script.js?71815548&data_platform=code&data_date=2017-10-02"
+        charset="utf-8"
+      ></script>
+      <script type="text/javascript">
+        xProductBrowser(
+          "categoriesPerRow=3",
+          "views=grid(20,3) list(60) table(60)",
+          "categoryView=grid",
+          "searchView=list",
+          "id=my-store-71815548"
+        );
+      </script>
+    </div>
+  </div>
+</template>

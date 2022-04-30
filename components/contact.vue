f1efaa24a4eb101645bb39e0cc7b6892aa50ec1e
Sanseli <lisasansen97@gmail.com>
2022-04-30 14:06:10 +0200

    wip mobile + contact

--- components/contact.vue
+++ components/contact.vue
@@ -2,10 +2,34 @@
   <div id="contact">
     <h1 style="text-align: center">CONTACT</h1>
 
-    <div style="display: flex">
+    <div style="display: flex; flex-direction: col">
       <div>
-        <h2>Openingsuren</h2>
-        <v-simple-table style="width: 20rem">
+        <h2><v-icon>mdi-clock-outline</v-icon> Openingsuren</h2>
+        <div style="display: grid">
+          <div
+            v-for="(item, index) in openingHours"
+            :key="item"
+            :style="{
+              'grid-column': 1,
+              'grid-row': index + 1,
+              'margin-right': '1rem',
+            }"
+          >
+            {{ item.day }}
+          </div>
+          <div
+            v-for="(item, index) in openingHours"
+            :key="item"
+            :style="{ 'grid-column': 2, 'grid-row': index + 1 }"
+          >
+            {{ item.hours }}
+          </div>
+          <!-- <div tyle="grid-row: 2" v-for="item in openingHours" :key="item">
+            {{ item.hours }}
+          </div> -->
+        </div>
+
+        <!-- <v-simple-table style="width: 20rem">
           <template v-slot:default>
             <tbody>
               <tr v-for="item in openingHours" :key="item.day">
@@ -14,10 +38,33 @@
               </tr>
             </tbody>
           </template>
-        </v-simple-table>
+        </v-simple-table> -->
       </div>
 
       <div>
+        <h2><v-icon>mdi-phone</v-icon> Contacteer ons</h2>
+        <a href="tel:+32471 95 1977">+32 (0)471 95 1977</a>
+      </div>
+
+      <div>
+        <h2><v-icon>mdi-phone</v-icon> Bezoek ons</h2>
+        <a href="https://goo.gl/maps/QbnRgaVU8LyE7Vx4A" target="_blank">
+          TakeAway De Snelle Hap <br />
+          Markt 5 <br />
+          3980 Tessenderlo
+        </a>
+        <br />
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2507.227821509602!2d5.0858278157538495!3d51.0673443795653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c1392d82bb3705%3A0x2cd03ac95764df59!2sDe%20Snelle%20Hap!5e0!3m2!1sen!2sbe!4v1651273454250!5m2!1sen!2sbe"
+          width="100"
+          height="100"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <!-- <div>
         <h2>Algemeen</h2>
         <a href="https://goo.gl/maps/QbnRgaVU8LyE7Vx4A" target="_blank"
           >Markt 5, 3980 Tessenderlo</a
@@ -26,16 +73,8 @@
       </div>
       <div>
         <h2>Map</h2>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2507.227821509602!2d5.0858278157538495!3d51.0673443795653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c1392d82bb3705%3A0x2cd03ac95764df59!2sDe%20Snelle%20Hap!5e0!3m2!1sen!2sbe!4v1651273454250!5m2!1sen!2sbe"
-          width="300"
-          height="300"
-          style="border: 0"
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div>
+       
+      </div> -->
     </div>
   </div>
 </template>
@@ -63,5 +102,11 @@ export default {
   padding: 5rem 2rem 2rem 2rem;
   background-color: #7e001eff;
   color: white;
+
+  i,
+  a {
+    color: white;
+    text-decoration: none;
+  }
 }
 </style>
--- components/menuCard/menuCardItems.vue
+++ components/menuCard/menuCardItems.vue
@@ -1,47 +1,55 @@
 <template>
-<div>
-  <div v-if="isNuxtReady">
-    <carousel
-      :margin="15"
-      :items="columns"
-      :stagePadding="10"
-      :dots="false"
-      :nav="false"
-      :slideValue="slideController"
-      :lazyLoad="true"
-    >
-      <v-card
-        v-for="item in menuItem.items"
-        :key="item.name"
-        class="menuItem"
-        width="300"
-        height="256"
+  <div>
+    <div v-if="isNuxtReady">
+      <carousel
+        :margin="15"
+        :stagePadding="10"
+        :slideValue="slideController"
+        :autoWidth="true"
+        :height="1"
+        :lazyLoad="true"
+        :nav="false"
+        :dots="false"
+        :rewind="false"
       >
-        <v-img style="background:white" height="80%" :src="`menu/${menuItem.slug}/${item.image}`"></v-img>
+        <v-card
+          v-for="item in menuItem.items"
+          :key="item.name"
+          class="menuItem"
+          :width="cardSize"
+          :height="cardSize"
+        >
+          <v-img
+            style="background: white"
+            height="80%"
+            :src="`menu/${menuItem.slug}/${item.image}`"
+          ></v-img>
 
-        <v-card-title class="title" style="color: white">{{ item.name }}</v-card-title>
-      </v-card>
-    </carousel>
+          <v-card-title class="title" style="color: white">{{
+            item.name
+          }}</v-card-title>
+        </v-card>
+      </carousel>
 
-    <div style="text-align: center">
-      <v-btn class="ma-2" text icon @click="slideController++">
-        <v-icon>mdi-chevron-left</i></v-icon>
-      </v-btn>
+      <div style="text-align: center">
+        <v-btn class="ma-2" text icon @click="slideController--">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
 
-      <v-btn class="ma-2" text icon @click="slideController--">
-        <v-icon>mdi-chevron-right</v-icon>
-      </v-btn>
+        <v-btn class="ma-2" text icon @click="slideController++">
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </div>
     </div>
-  </div>
-  
-  <div v-else class="skeletonLoaderRow">
-    <div v-for="column in columns" :key="column">
-    <v-skeleton-loader
-      type="card"
-      style="width: 275.04px; height: 300px; margin-right: 15px;"
-    ></v-skeleton-loader>
+
+    <div v-else class="skeletonLoaderRow">
+      <div v-for="column in columns" :key="column">
+        <v-skeleton-loader
+          type="card"
+          style="width: 275.04px; height: 300px; margin-right: 15px"
+        ></v-skeleton-loader>
+      </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -60,7 +68,7 @@ export default {
   props: {
     menuItem: {
       type: Object,
-      required: true
+      required: true,
     },
   },
   mounted() {
@@ -73,9 +81,9 @@ export default {
   },
   computed: {
     columns() {
-      // if (this.$vuetify.breakpoint.xl) {
-      //   return 4;
-      // }
+      if (this.$vuetify.breakpoint.xl) {
+        return 7;
+      }
 
       if (this.$vuetify.breakpoint.lg) {
         return 5;
@@ -90,37 +98,54 @@ export default {
       // }
 
       return 2;
-    }
-  }
+    },
+
+    cardSize() {
+      // if (this.$vuetify.breakpoint.xl) {
+      //   return 7;
+      // }
+
+      // if (this.$vuetify.breakpoint.lg) {
+      //   return 5;
+      // }
+
+      // if (this.$vuetify.breakpoint.md) {
+      //   return ;
+      // }
+
+      if (this.$vuetify.breakpoint.sm) {
+        return 140;
+      }
+
+      return 256;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
 .menuItem {
-  background-color: #b86c30 !important; 
+  background-color: #b86c30 !important;
 
   .title {
     font-size: 1em !important;
-    
-    
   }
-.v-card__title {
-      padding: 9px !important;
-    }
-  
+  .v-card__title {
+    padding: 9px !important;
+  }
 }
 .v-skeleton-loader__card-heading {
-    background-color: #b86c30 !important;
-  }
-  .v-skeleton-loader__image {
-    background-color: white !important;
-  }
+  background-color: #b86c30 !important;
+}
+.v-skeleton-loader__image {
+  background-color: white !important;
+}
 .spaceBetween > * + * {
   margin-left: 0.5rem;
 }
 
 .skeletonLoaderRow {
   display: flex !important;
-  padding: 0 10px 0 10px ;
+  padding: 0 10px 0 10px;
 }
 </style>
